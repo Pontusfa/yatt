fbbbfe18ff6219741d0d27b34477c6f5fa115613
Pontus Falk <pontusfa@kth.se>
2013-10-23 21:14:18 +0200

    Login prework.

--- controllers/index/controller.js
+++ controllers/index/controller.js
@@ -4,18 +4,27 @@
  * @version 0.0.1
  */
 
+
+
+
 function _getIndex(req, res){
-    res.write('token: )' + req.locals.csrfToken);
     res.end();
 }
 
 function _getRoot(req, res){
     res.redirect('/index');
 }
+function _postIndex(req, res){
+    req.session.username = req.body.username;
+    req.session.password = req.body.password;
+    res.write('logged in');
+    res.end();
+}
 
 function setup(app){
     app.get('/', _getRoot);
     app.get('/index', _getIndex);
+    app.post('/index', _postIndex);
     return true;
 }
 
