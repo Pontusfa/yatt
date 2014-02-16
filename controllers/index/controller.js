8827c6dd0b5d9e237ceb628a5c5525fc89f0b8db
Pontus Falk <pontusfa@kth.se>
2014-02-16 15:26:08 +0100

    Minor adjustments to... Everything.

--- controllers/index/controller.js
+++ controllers/index/controller.js
@@ -5,19 +5,20 @@
  */
 
 
-
-
 function _getIndex(req, res){
+    req.session.cnt = req.session.cnt+3 || 1;
+    res.write('lo ' + req.session.cnt);
     res.end();
 }
 
 function _getRoot(req, res){
     res.redirect('/index');
 }
+
 function _postIndex(req, res){
     req.session.username = req.body.username;
     req.session.password = req.body.password;
-    res.write('logged in');
+    res.write('logged in, ' + req.session.username);
     res.end();
 }
 
